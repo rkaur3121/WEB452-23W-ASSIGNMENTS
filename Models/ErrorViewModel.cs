573f9e72c7cb0216b5d392ce58d6eeab1030d8e2
rkaur3121 <115298724+rkaur3121@users.noreply.github.com>
2023-02-16 21:32:01 -0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Magnets.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
