a08a6f19cfe211898e94e2fd5ef776f9a14b95ab
rkaur3121 <115298724+rkaur3121@users.noreply.github.com>
2023-02-16 22:12:07 -0500

    added MagnetContext.cs class in Data folder

---
+++ Data/MagnetContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Magnets.Models;
+
+namespace Magnets.Data
+{
+    public class MagnetContext : DbContext
+    {
+        public MagnetContext(DbContextOptions<MagnetContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Magnet> Movie { get; set; }
+    }
+}
